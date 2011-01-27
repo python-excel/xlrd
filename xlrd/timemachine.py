f5faae5e8145dc65114cb3903559a29c017a388f
sjmachin <sjmachin@ac150577-6901-3246-a117-cd8547e6756f>
2011-01-27 21:23:58 +0000

    Made test for array module independent of IronPython
    git-svn-id: https://secure.simplistix.co.uk/svn/xlrd/trunk@4590 ac150577-6901-3246-a117-cd8547e6756f

--- xlrd/timemachine.py
+++ xlrd/timemachine.py
@@ -1,13 +1,14 @@
 # -*- coding: cp1252 -*-
 
 ##
-# <p>Copyright © 2006-2009 Stephen John Machin, Lingfo Pty Ltd</p>
+# <p>Copyright © 2006-2011 Stephen John Machin, Lingfo Pty Ltd</p>
 # <p>This module is part of the xlrd package, which is released under a BSD-style licence.</p>
 ##
 
 # timemachine.py -- adaptation for earlier Pythons e.g. 2.1
 # usage: from timemachine import *
 
+# 2011-01-19 SJM Made test for array module independent of IronPython
 # 2008-02-08 SJM Generalised method of detecting IronPython
 from __future__ import nested_scopes
 import sys
@@ -17,10 +18,11 @@ python_version = sys.version_info[:2] # e.g. version 2.4 -> (2, 4)
 CAN_PICKLE_ARRAY = python_version >= (2, 5)
 CAN_SUBCLASS_BUILTIN = python_version >= (2, 2)
 
-if sys.version.find("IronPython") >= 0:
-    array_array = None
-else:
+try:
     from array import array as array_array
+except ImportError:
+    # old version of IronPython?
+    array_array = None
 
 if python_version < (2, 2):
     class object:
