e267a7d7862f243ff2a57be6547b548d533df638
Thomas Kluyver <takowl@gmail.com>
2012-03-10 18:44:15 +0000

    Add script to run tests.

---
+++ tests/run_tests.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import unittest
+
+from test_cell import *
+from test_formats import *
+from test_formulas_sjmachin import *
+from test_sheet import *
+from test_workbook import *
+#from test_xfcell import * # Not currently working
+from test_xldate import *
+
+if __name__ == "__main__":
+    unittest.main()
