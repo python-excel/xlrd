8c84fda3033086aa0119488646f7e20a90fb8e39
Thomas Kluyver <takowl@gmail.com>
2012-04-04 11:32:03 +0100

    Remove non-corresponding xfcell tests.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -7,7 +7,6 @@ from test_formats import *
 from test_formulas_sjmachin import *
 from test_sheet import *
 from test_workbook import *
-#from test_xfcell import * # Not currently working
 from test_xldate import *
 
 if __name__ == "__main__":
