a4cf012fada82a44fcaf7b141de96b8377530fbc
Thomas Kluyver <takowl@gmail.com>
2013-01-06 11:51:40 +0000

    Add test for biffh.hex_char_dump()

---
+++ tests/test_biffh.py
@@ -0,0 +1,22 @@
+import unittest
+import sys
+
+if sys.version_info[0] >= 3:
+    from io import StringIO
+else:
+    # Python 2.6+ does have the io module, but io.StringIO is strict about
+    # unicode, which won't work for our test.
+    from StringIO import StringIO
+
+from xlrd import biffh
+
+class TestHexDump(unittest.TestCase):
+    def test_hex_char_dump(self):
+        sio = StringIO()
+        biffh.hex_char_dump(b"abc\0e\01", 0, 6, fout=sio)
+        s = sio.getvalue()
+        assert "61 62 63 00 65 01" in s, s
+        assert "abc~e?" in s, s
+
+if __name__=='__main__':
+    unittest.main()
