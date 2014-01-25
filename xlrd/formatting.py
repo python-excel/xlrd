python-excel/xlrd

commit 83b96434be90ea179bb55abc2802b3d568a7dfc7

`fprintf` was defined in `timemachine` and `biffh` with different functionality. Now have only one definition, in `timemachine`. Changed many occurrences of `print("%r" % x, file=f)` to `fprintf(f, "%r\n", x)` so that they would use %a instead of %r in py3.

xlrd/formatting.py
@@ -479,7 +479,7 @@ def is_date_format_string(book, fmt):
             state = 0
         assert 0 <= state <= 2
     if book.verbosity >= 4:
-        print("is_date_format_string: reduced format is %r" % s, file=book.logfile)
+        print("is_date_format_string: reduced format is %s" % REPR(s), file=book.logfile)
     s = fmt_bracketed_sub('', s)
     if s in non_date_formats:
         return False
@@ -614,8 +614,8 @@ def palette_epilogue(book):
             book.colour_indexes_used[cx] = 1
         elif book.verbosity:
             print("Size of colour table:", len(book.colour_map), file=book.logfile)
-            print("*** Font #%d (%r): colour index 0x%04x is unknown" \
-                % (font.font_index, font.name, cx), file=book.logfile)
+            fprintf(self.logfile, "*** Font #%d (%r): colour index 0x%04x is unknown\n",
+                font.font_index, font.name, cx)
     if book.verbosity >= 1:
         used = sorted(book.colour_indexes_used.keys())
         print("\nColour indexes used:\n%r\n" % used, file=book.logfile)
@@ -661,8 +661,8 @@ def handle_style(book, data):
             print("WARNING *** A user-defined style has a zero-length name", file=book.logfile)
     book.style_name_map[name] = (built_in, xf_index)
     if blah:
-        print("STYLE: built_in=%d xf_index=%d built_in_id=%d level=%d name=%r" \
-            % (built_in, xf_index, built_in_id, level, name), file=book.logfile)
+        fprintf(book.logfile, "STYLE: built_in=%d xf_index=%d built_in_id=%d level=%d name=%r\n",
+            built_in, xf_index, built_in_id, level, name)
 
 def check_colour_indexes_in_obj(book, obj, orig_index):
     alist = sorted(obj.__dict__.items())

xlrd/biffh.py
@@ -65,7 +65,7 @@ class BaseObject(object):
                 ):
                 print("%s%s: %s, len = %d" % (pad, attr, type(value), len(value)), file=f)
             else:
-                print("%s%s: %r" % (pad, attr, value), file=f)
+                fprintf(f, "%s%s: %r\n", pad, attr, value)
         if footer is not None: print(footer, file=f)
 
 FUN, FDT, FNU, FGE, FTX = range(5) # unknown, date, number, general, text
@@ -254,14 +254,6 @@ for _cell_opcode in _cell_opcode_list:
 
 def is_cell_opcode(c):
     return c in  _cell_opcode_dict
-
-# def fprintf(f, fmt, *vargs): f.write(fmt % vargs)
-
-def fprintf(f, fmt, *vargs):
-    if fmt.endswith('\n'):
-        print(fmt[:-1] % vargs, file=f)
-    else:
-        print(fmt % vargs, end=' ', file=f)
 
 def upkbits(tgt_obj, src, manifest, local_setattr=setattr):
     for n, mask, attr in manifest:

xlrd/book.py
@@ -966,9 +966,11 @@ class Book(BaseObject):
         nobj.excel_sheet_index = sheet_index
         nobj.scope = None # patched up in the names_epilogue() method
         if blah:
-            print("NAME[%d]:%s oflags=%d, name_len=%d, fmla_len=%d, extsht_index=%d, sheet_index=%d, name=%r" \
-                % (name_index, macro_flag, option_flags, name_len,
-                fmla_len, extsht_index, sheet_index, internal_name), file=self.logfile)
+            fprintf(
+                self.logfile,
+                "NAME[%d]:%s oflags=%d, name_len=%d, fmla_len=%d, extsht_index=%d, sheet_index=%d, name=%r\n",
+                name_index, macro_flag, option_flags, name_len,
+                fmla_len, extsht_index, sheet_index, internal_name)
         name = internal_name
         if nobj.builtin:
             name = builtin_name_from_code.get(name, "??Unknown??")
@@ -989,9 +991,9 @@ class Book(BaseObject):
         f = self.logfile
         if blah:
             print("+++++ names_epilogue +++++", file=f)
-            print("_all_sheets_map", self._all_sheets_map, file=f)
-            print("_extnsht_name_from_num", self._extnsht_name_from_num, file=f)
-            print("_sheet_num_from_name", self._sheet_num_from_name, file=f)
+            print("_all_sheets_map", REPR(self._all_sheets_map), file=f)
+            print("_extnsht_name_from_num", REPR(self._extnsht_name_from_num), file=f)
+            print("_sheet_num_from_name", REPR(self._sheet_num_from_name), file=f)
         num_names = len(self.name_obj_list)
         for namex in range(num_names):
             nobj = self.name_obj_list[namex]
@@ -1093,10 +1095,10 @@ class Book(BaseObject):
         url, pos = unpack_unicode_update_pos(data, 2, lenlen=2)
         if num_sheets == 0:
             self._supbook_types[-1] = SUPBOOK_DDEOLE
-            if blah: print("SUPBOOK[%d]: DDE/OLE document = %r" % (sbn, url), file=self.logfile)
+            if blah: fprintf(self.logfile, "SUPBOOK[%d]: DDE/OLE document = %r\n", sbn, url)
             return
         self._supbook_types[-1] = SUPBOOK_EXTERNAL
-        if blah: print("SUPBOOK[%d]: url = %r" % (sbn, url), file=self.logfile)
+        if blah: fprintf(self.logfile, "SUPBOOK[%d]: url = %r\n", sbn, url)
         sheet_names = []
         for x in range(num_sheets):
             try:
@@ -1111,7 +1113,7 @@ class Book(BaseObject):
                         ), file=self.logfile)
                 break
             sheet_names.append(shname)
-            if blah: print("  sheetx=%d namelen=%d name=%r (next pos=%d)" % (x, len(shname), shname, pos), file=self.logfile)
+            if blah: fprintf(self.logfile, "  sheetx=%d namelen=%d name=%r (next pos=%d)\n", x, len(shname), shname, pos)
 
     def handle_sheethdr(self, data):
         # This a BIFF 4W special.
@@ -1126,7 +1128,7 @@ class Book(BaseObject):
         self._sheethdr_count += 1
         BOF_posn = self._position
         posn = BOF_posn - 4 - len(data)
-        if DEBUG: print('SHEETHDR %d at posn %d: len=%d name=%r' % (sheetno, posn, sheet_len, sheet_name), file=self.logfile)
+        if DEBUG: fprintf(self.logfile, 'SHEETHDR %d at posn %d: len=%d name=%r\n', sheetno, posn, sheet_len, sheet_name)
         self.initialise_format_info()
         if DEBUG: print('SHEETHDR: xf epilogue flag is %d' % self._xf_epilogue_done, file=self.logfile)
         self._sheet_list.append(None) # get_sheet updates _sheet_list but needs a None beforehand
@@ -1166,7 +1168,7 @@ class Book(BaseObject):
             print("SST processing took %.2f seconds" % (t1 - t0, ), file=self.logfile)
 
     def handle_writeaccess(self, data):
-        # DEBUG = 0
+        DEBUG = 0
         if self.biff_version < 80:
             if not self.encoding:
                 self.raw_user_name = True
@@ -1175,7 +1177,7 @@ class Book(BaseObject):
             strg = unpack_string(data, 0, self.encoding, lenlen=1)
         else:
             strg = unpack_unicode(data, 0, lenlen=2)
-        if DEBUG: print("WRITEACCESS: %d bytes; raw=%d %r" % (len(data), self.raw_user_name, strg), file=self.logfile)
+        if DEBUG: fprintf(self.logfile, "WRITEACCESS: %d bytes; raw=%s %r\n", len(data), self.raw_user_name, strg)
         strg = strg.rstrip()
         self.user_name = strg
 
@@ -1223,8 +1225,8 @@ class Book(BaseObject):
             elif rc == XL_STYLE:
                 self.handle_style(data)
             elif rc & 0xff == 9 and self.verbosity:
-                print("*** Unexpected BOF at posn %d: 0x%04x len=%d data=%r" \
-                    % (self._position - length - 4, rc, length, data), file=self.logfile)
+                fprintf(self.logfile, "*** Unexpected BOF at posn %d: 0x%04x len=%d data=%r\n",
+                    self._position - length - 4, rc, length, data)
             elif rc ==  XL_EOF:
                 self.xf_epilogue()
                 self.names_epilogue()
@@ -1269,7 +1271,7 @@ class Book(BaseObject):
                 % (length, opcode))
         padding = b'\0' * max(0, boflen[opcode] - length)
         data = self.read(self._position, length);
-        if DEBUG: print("\ngetbof(): data=%r" % data, file=self.logfile)
+        if DEBUG: fprintf(self.logfile, "\ngetbof(): data=%r\n", data)
         if len(data) < length:
             bof_error('Incomplete BOF record[2]; met end of file')
         data += padding

xlrd/compdoc.py
@@ -56,9 +56,11 @@ class DirNode(object):
             self.dump(DEBUG)
 
     def dump(self, DEBUG=1):
-        print("DID=%d name=%r etype=%d DIDs(left=%d right=%d root=%d parent=%d kids=%r) first_SID=%d tot_size=%d" \
-            % (self.DID, self.name, self.etype, self.left_DID,
-            self.right_DID, self.root_DID, self.parent, self.children, self.first_SID, self.tot_size), file=self.logfile)
+        fprintf(
+            "DID=%d name=%r etype=%d DIDs(left=%d right=%d root=%d parent=%d kids=%r) first_SID=%d tot_size=%d\n",
+            self.DID, self.name, self.etype, self.left_DID,
+            self.right_DID, self.root_DID, self.parent, self.children, self.first_SID, self.tot_size
+            )
         if DEBUG == 2:
             # cre_lo, cre_hi, mod_lo, mod_hi = tsinfo
             print("timestamp info", self.tsinfo, file=self.logfile)
@@ -323,9 +325,9 @@ class CompDoc(object):
                         )
             assert s == EOCSID
             if todo != 0:
-                print("WARNING *** OLE2 stream %r: expected size %d, actual size %d" \
-                    % (name, size, size - todo), file=self.logfile)
-        # print >> self.logfile, "_get_stream(%s): seen" % name; dump_list(self.seen, 20, self.logfile)
+                fprintf(self.logfile, 
+                    "WARNING *** OLE2 stream %r: expected size %d, actual size %d\n",
+                    name, size, size - todo)
 
         return b''.join(sectors)
 

xlrd/formula.py
@@ -1327,8 +1327,8 @@ def evaluate_name_formula(bk, nobj, namex, blah=0, level=0):
         pos += sz
     any_rel = not not any_rel
     if blah:
-        print("End of formula. level=%d any_rel=%d any_err=%d stack=%r" % \
-            (level, not not any_rel, any_err, stack), file=bk.logfile)
+        fprintf(bk.logfile, "End of formula. level=%d any_rel=%d any_err=%d stack=%r\n",
+            level, not not any_rel, any_err, stack)
         if len(stack) >= 2:
             print("*** Stack has unprocessed args", file=bk.logfile)
         print(file=bk.logfile)

xlrd/sheet.py
@@ -1,7 +1,7 @@
 # -*- coding: cp1252 -*-
 
 ##
-# <p> Portions copyright © 2005-2012 Stephen John Machin, Lingfo Pty Ltd</p>
+# <p> Portions copyright © 2005-2013 Stephen John Machin, Lingfo Pty Ltd</p>
 # <p>This module is part of the xlrd package, which is released under a BSD-style licence.</p>
 ##
 
@@ -1725,7 +1725,7 @@ class Sheet(BaseObject):
         if (options & 1) and not (options & 0x100): # HasMoniker and not MonikerSavedAsString
             # an OLEMoniker structure
             clsid, = unpack('<16s', data[offset:offset + 16])
-            if DEBUG: print("clsid=%r" %clsid, file=self.logfile)
+            if DEBUG: fprintf(self.logfile, "clsid=%r\n",  clsid)
             offset += 16
             if clsid == b"\xE0\xC9\xEA\x79\xF9\xBA\xCE\x11\x8C\x82\x00\xAA\x00\x4B\xA9\x0B":
                 #          E0H C9H EAH 79H F9H BAH CEH 11H 8CH 82H 00H AAH 00H 4BH A9H 0BH
@@ -1734,7 +1734,7 @@ class Sheet(BaseObject):
                 nbytes = unpack('<L', data[offset:offset + 4])[0]
                 offset += 4
                 h.url_or_path = unicode(data[offset:offset + nbytes], 'UTF-16le')
-                if DEBUG: print("initial url=%r len=%d" % (h.url_or_path, len(h.url_or_path)), file=self.logfile)
+                if DEBUG: fprintf(self.logfile, "initial url=%r len=%d\n", h.url_or_path, len(h.url_or_path))
                 endpos = h.url_or_path.find('\x00')
                 if DEBUG: print("endpos=%d" % endpos, file=self.logfile)
                 h.url_or_path = h.url_or_path[:endpos]
@@ -1743,9 +1743,12 @@ class Sheet(BaseObject):
                 extra_nbytes = nbytes - true_nbytes
                 extra_data = data[offset:offset + extra_nbytes]
                 offset += extra_nbytes
-                if DEBUG: print("url=%r" % h.url_or_path, file=self.logfile)
-                if DEBUG: print("extra=%r" % extra_data, file=self.logfile)
-                if DEBUG: print("nbytes=%d true_nbytes=%d extra_nbytes=%d" % (nbytes, true_nbytes, extra_nbytes), file=self.logfile)
+                if DEBUG: 
+                    fprintf(
+                        self.logfile,
+                        "url=%r\nextra=%r\nnbytes=%d true_nbytes=%d extra_nbytes=%d\n",
+                        h.url_or_path, extra_data, nbytes, true_nbytes, extra_nbytes,
+                        )
                 assert extra_nbytes in (24, 0)
             elif clsid == b"\x03\x03\x00\x00\x00\x00\x00\x00\xC0\x00\x00\x00\x00\x00\x00\x46":
                 # file moniker
@@ -1753,7 +1756,7 @@ class Sheet(BaseObject):
                 uplevels, nbytes = unpack("<Hi", data[offset:offset + 6])
                 offset += 6
                 shortpath = b"..\\" * uplevels + data[offset:offset + nbytes - 1] #### BYTES, not unicode
-                if DEBUG: print("uplevels=%d shortpath=%r" % (uplevels, shortpath), file=self.logfile)
+                if DEBUG: fprintf(self.logfile, "uplevels=%d shortpath=%r\n", uplevels, shortpath)
                 offset += nbytes
                 offset += 24 # OOo: "unknown byte sequence"
                 # above is version 0xDEAD + 20 reserved zero bytes
@@ -1772,7 +1775,7 @@ class Sheet(BaseObject):
                     #### MS KLUDGE WARNING ####
                     # The "shortpath" is bytes encoded in the **UNKNOWN** creator's "ANSI" encoding.
             else:
-                print("*** unknown clsid %r" % clsid, file=self.logfile)
+                fprintf(self.logfile, "*** unknown clsid %r\n", clsid)
         elif options & 0x163 == 0x103: # UNC
             h.type = UNICODE_LITERAL('unc')
             h.url_or_path, offset = get_nul_terminated_unicode(data, offset)
@@ -2034,7 +2037,7 @@ class Sheet(BaseObject):
         assert rt == 0x872
         assert fHdr == 0
         assert Ref1 == Ref0
-        print("FEAT11: grbitFrt=%d  Ref0=%r cref=%d cbFeatData=%d" % (grbitFrt, Ref0, cref, cbFeatData), file=self.logfile)
+        print(self.logfile, "FEAT11: grbitFrt=%d  Ref0=%r cref=%d cbFeatData=%d\n", grbitFrt, Ref0, cref, cbFeatData)
         # lt: Table data source type:
         #   =0 for Excel Worksheet Table =1 for read-write SharePoint linked List
         #   =2 for XML mapper Table =3 for Query Table

xlrd/timemachine.py
@@ -10,6 +10,7 @@
 # usage: from timemachine import *
 
 from __future__ import nested_scopes
+from __future__ import print_function
 import sys
 
 python_version = sys.version_info[:2] # e.g. version 2.4 -> (2, 4)
@@ -24,7 +25,10 @@ if python_version >= (3, 0):
     from io import BytesIO as BYTES_IO
     def fprintf(f, fmt, *vargs):
         fmt = fmt.replace("%r", "%a")
-        f.write(fmt % vargs)
+        if fmt.endswith('\n'):
+            print(fmt[:-1] % vargs, file=f)
+        else:
+            print(fmt % vargs, end=' ', file=f)        
     EXCEL_TEXT_TYPES = (str, bytes, bytearray) # xlwt: isinstance(obj, EXCEL_TEXT_TYPES)
     REPR = ascii
     xrange = range
@@ -38,7 +42,10 @@ else:
     BYTES_ORD = ord
     from cStringIO import StringIO as BYTES_IO
     def fprintf(f, fmt, *vargs):
-        f.write(fmt % vargs)
+        if fmt.endswith('\n'):
+            print(fmt[:-1] % vargs, file=f)
+        else:
+            print(fmt % vargs, end=' ', file=f)        
     try:
         EXCEL_TEXT_TYPES = basestring # xlwt: isinstance(obj, EXCEL_TEXT_TYPES)
     except NameError:

xlrd/xlsx.py
@@ -440,7 +440,7 @@ class X12SST(X12General):
             self.dumpout('Entries in SST: %d', len(sst))
         if self.verbosity >= 3:
             for x, s in enumerate(sst):
-                print("SST x=%d s=%r" % (x, s))
+                fprintf(self.logfile, "SST x=%d s=%r\n", x, s)
 
     def process_stream_findall(self, stream, heading=None):
         if self.verbosity >= 2 and heading is not None:

commit 7e7af4fb1bb74480fc66a771b3ced811370b4af8

Fix variable name error introduced in commit 83b9643

xlrd/formatting.py
@@ -614,7 +614,7 @@ def palette_epilogue(book):
             book.colour_indexes_used[cx] = 1
         elif book.verbosity:
             print("Size of colour table:", len(book.colour_map), file=book.logfile)
-            fprintf(self.logfile, "*** Font #%d (%r): colour index 0x%04x is unknown\n",
+            fprintf(book.logfile, "*** Font #%d (%r): colour index 0x%04x is unknown\n",
                 font.font_index, font.name, cx)
     if book.verbosity >= 1:
         used = sorted(book.colour_indexes_used.keys())