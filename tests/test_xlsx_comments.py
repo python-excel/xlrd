25012f229dd9d23661690151ae51c0b329b8068a
Gunnlaugur Þór Briem <gunnlaugur@gmail.com>
2014-06-24 18:28:45 +0000

    Fix xlsx sheet->comments relationship
    Instead of the previous bogus assumption that `xl/commentsN.xml` always
    referred to `xl/worksheets/sheetN.xml`, now find the correct comments
    file for a given sheet via `xl/worksheets/_rels/sheetN.xml.rels`
    
    Closes python-excel/xlrd#108

--- tests/test_xlsx_comments.py
+++ tests/test_xlsx_comments.py
@@ -44,3 +44,12 @@ class TestXlsxComments(TestCase):
         note_map = sheet.cell_note_map
         self.assertEqual(len(note_map), 1)
         self.assertEqual(note_map[(0, 1)].text, 'Just a test')
+
+    def test_excel_comments_with_multi_sheets(self):
+        book = open_workbook(from_this_dir('test_comments_excel_sheet2.xlsx'))
+        sheet = book.sheet_by_index(1)
+
+        note_map = sheet.cell_note_map
+        self.assertEqual(len(note_map), 1)
+        self.assertEqual(note_map[(1, 1)].text, 'Note lives here')
+        self.assertEqual(len(book.sheet_by_index(0).cell_note_map), 0)
