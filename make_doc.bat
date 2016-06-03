f536afa8d01c4a2c0495325d4f562ccdee9fa549
Chris Withers <chris@simplistix.co.uk>
2016-06-03 00:05:17 -0700

    remove the old doc tools themselves.

--- make_doc.bat
+++
@@ -1,6 +0,0 @@
-c:\python25\python pkg_doc.py xlrd
-c:\python25\python pythondoc.py xlrd/compdoc.py
-del compdoc.html
-rename pythondoc-compdoc.html compdoc.html
-copy xlrd.html xlrd\doc
-copy compdoc.html xlrd\doc
