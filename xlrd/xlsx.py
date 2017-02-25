9b6d28784014d7794ef33e22426c0b3a824fba1f
dronovdmitry <dronov.dmitry@gmail.com>
2017-02-25 09:31:52 +0200

    Fix IronPython with russian Letters
    Fix IronPython with russian Letters
    -
    To Start xlrd from IronPython with russian language: https://goo.gl/Zz8U9x 
    Need to fix xmllib at 95 line. Change __accept_utf8 = 1 to 0 : https://goo.gl/y3vZTV

--- xlrd/xlsx.py
+++ xlrd/xlsx.py
@@ -43,7 +43,10 @@ def ensure_elementtree_imported(verbosity, logfile):
                         except ImportError:
                             raise Exception("Failed to import an ElementTree implementation")
     if hasattr(ET, 'iterparse'):
-        _dummy_stream = BYTES_IO(b'')
+        if 'IronPython' in sys.version:
+            _dummy_stream = BYTES_IO('')
+        else:
+            _dummy_stream = BYTES_IO(b'')
         try:
             ET.iterparse(_dummy_stream)
             ET_has_iterparse = True
@@ -140,7 +143,11 @@ def cooked_text(self, elem):
         return ''
     if elem.get(XML_SPACE_ATTR) != 'preserve':
         t = t.strip(XML_WHITESPACE)
-    return ensure_unicode(unescape(t))
+    unsc = unescape(t)
+    if 'IronPython'  in sys.version:
+        return (unsc.decode('utf-8'))
+    else:
+        return ensure_unicode(unsc)
 
 def get_text_from_si_or_is(self, elem, r_tag=U_SSML12+'r', t_tag=U_SSML12 +'t'):
     "Returns unescaped unicode"
@@ -483,7 +490,11 @@ class X12Styles(X12General):
         self.xf_type = 1
 
     def do_numfmt(self, elem):
-        formatCode = ensure_unicode(elem.get('formatCode'))
+        el_get = elem.get('formatCode')
+        if 'IronPython' in sys.version:
+            formatCode = (el_get.decode('utf-8'))
+        else:
+            formatCode = ensure_unicode(el_get)
         numFmtId = int(elem.get('numFmtId'))
         is_date = is_date_format_string(self.bk, formatCode)
         self.fmt_is_date[numFmtId] = is_date
