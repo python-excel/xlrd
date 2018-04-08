5eacd93b9622b54db4904050e4cef1ad0c165ad1
Ravi Kumar L <upman16@gmail.com>
2018-04-08 23:46:03 +0530

    Add test for tilde path expansion

--- tests/base.py
+++ tests/base.py
@@ -1,5 +1,14 @@
 import os
+import shutil
 
 
 def from_this_dir(filename):
     return os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+
+def from_home_dir(filename, fn):
+    abs_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), filename)
+    home_path = os.path.expanduser( os.path.join('~', filename))
+
+    shutil.copyfile(abs_path, home_path)
+    fn(home_path)
+    shutil.os.remove(home_path)
