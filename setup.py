687d9715854ff5300896be99ff5ef3761d306ae0
Chris Withers <chris@simplistix.co.uk>
2020-12-10 08:57:23 +0000

    update target versions

--- setup.py
+++ setup.py
@@ -19,8 +19,8 @@ setup(
     long_description = (
         "Extract data from Excel spreadsheets "
         "(.xls and .xlsx, versions 2.0 onwards) on any platform. "
-        "Pure Python (2.7, 3.4+). "
         "Strong support for Excel dates. Unicode-aware."
+        "Pure Python (2.7, 3.6+). "
     ),
     platforms = ["Any platform -- don't need Windows"],
     license = 'BSD',
@@ -33,14 +33,14 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'Operating System :: OS Independent',
         'Topic :: Database',
         'Topic :: Office/Business',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
 )
