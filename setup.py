22e5b442194c09f9f9c8ce58ae1092de78e18e3d
Chris Withers <chris@simplistix.co.uk>
2020-12-11 00:15:16 +0000

    RTD config

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
     python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*, !=3.5.*",
     extras_require=dict(
         test=['pytest', 'pytest-cov'],
+        docs=['sphinx'],
         build=['wheel', 'twine']
     )
 )
